8599c6ebf61f839c71d88a5c31a7308c0e9ee58d
Doug <wanderlustking11@gmail.com>
2025-04-21 13:48:19 -0400

    Simplify testing setup

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,10 +1,17 @@
 
-import { it, expect, describe } from 'vitest';
+// Can replace 'vitest' import by settign globals as 'true' in vitest.config.ts'
+// Must also update the tsconfig.json file and add 'vitest/globals' types to compilerOptions
+// import { it, expect, describe } from 'vitest';
+
+// Use Extension 'Testing Library Snippets': type 'itr'
 import { render, screen } from '@testing-library/react';
 import Greet from '../../src/components/Greet';
-import '@testing-library/jest-dom/vitest';
+
+// move this import into our /tests/setup.ts file
+// import '@testing-library/jest-dom/vitest';
 
 
+// Using the 'Vitest Snippets' extension, can type: d -> describe; i -> it; ex -> expect
 describe('Greet', () => {
     it('should render "Hello" with the name when name is provided', () => {
         render(<Greet name="Doug" />);
